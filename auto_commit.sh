5cf8a6c34ce8387b3b8b9231b9d253b0fb1a7167
Tiagos2k2 <tiagos.desktop@gmail.com>
2024-04-23 15:21:53 -0300

    Auto commit Tue, Apr 23, 2024  3:21:53 PM

--- auto_commit.sh
+++ auto_commit.sh
@@ -3,13 +3,18 @@
 # Caminho do diretório do seu projeto
 DIR="/c/Users/tiago.silva.DESKTOP/Documents/Tiago Nunes/Desenvolvimento/Estudos/Webdesign Front-end"
 
-cd "$DIR"
+cd $DIR
 
-# Adiciona todas as alterações
-git add .
+# Verifica se há mudanças locais
+if [[ $(git status --short) ]]; then
+    # Adiciona todas as alterações
+    git add .
 
-# Commit com uma mensagem automática
-git commit -m "Auto commit $(date)"
+    # Commit com uma mensagem automática
+    git commit -m "Auto commit $(date)"
 
-# Push para o repositório remoto
-git push origin main
+    # Push para o repositório remoto
+    git push origin main
+else
+    echo "No local changes to commit."
+fi
