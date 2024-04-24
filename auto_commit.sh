8c8228d431bc9748811da503989dcdd8025f9787
ConecTismo <tiagonunes2k2@gmail.com>
2024-04-24 02:23:10 -0300

    Auto commit qua, 24 de abr de 2024 02:23:10

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Caminho do diretório do seu projeto
-DIR="/c/Users/tiago.silva.DESKTOP/Documents/Tiago Nunes/Desenvolvimento/Estudos/Webdesign Front-end"
+DIR="/c/Users/GAMERX/Documents/ESTUDOS/Webdesign-Front-end"
 
 cd $DIR
 
